6dbfdc4af2c6f369c20dad4a2a5fa470e5250034
Abhishek T R <abhiabhishektr@gmail.com>
2025-11-03 13:20:41 +0530

    refactor: simplify database connection by removing unused Pool implementation

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,22 @@
 import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-  ssl: { rejectUnauthorized: false }, // required for Render
-});
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
 
-// Test connection
-pool.connect()
-  .then(() => console.log("✅ Database connected"))
-  .catch((err) => console.error("❌ DB connection error:", err));
 
-export const db = drizzle(pool, { schema });
+
+// import * as schema from "./schema";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL!,
+//   ssl: { rejectUnauthorized: false }, // required for Render
+// });
+
+// // Test connection
+// pool.connect()
+//   .then(() => console.log("✅ Database connected"))
+//   .catch((err) => console.error("❌ DB connection error:", err));
+
+// export const db = drizzle(pool, { schema });
